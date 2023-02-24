e8fe11ef32b1ade155e463890ee8e8885edbbeb9
Atley St. Vrain <atleystvrain@gmail.com>
2023-02-24 12:31:09 -0600

    started...

--- SelfAvoidingWalk.java
+++ SelfAvoidingWalk.java
@@ -1,7 +1,15 @@
 public class SelfAvoidingWalk {
 
     public static void printPathLengths(int n, int trials) {
-        
+        boolean[][] grid = new boolean[n][n];
+        int x = n/2;
+        int y = n/2;
+
+        while (x >= 0 && x < n && y >= 0 && y < n && grid[x][y]) {
+            grid[x][y] = false;
+
+            
+        }
     }
 
     public static void main(String[] args) {
